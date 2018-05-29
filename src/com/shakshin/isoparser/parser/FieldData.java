3067203d827f98938bb879a3c98a8a206d165d73
Sergey V. Shakshin <s.shakshin@compassplus.com>
2018-05-29 17:53:57 +0500

    fixes

--- src/com/shakshin/isoparser/parser/FieldData.java
+++ src/com/shakshin/isoparser/parser/FieldData.java
@@ -24,9 +24,6 @@ public class FieldData {
 
         }
 
-        if (cfg.raw && rawData != null)
-            res += "\nRAW: " + Utils.bin2hex(rawData) +
-                    "\n-------------------------------------------------------";
         return res;
     }
 }
